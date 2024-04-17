af5f6fd91fc6482b9332ab4fee3a1bc0c389be6d
Apeksha Bhausaheb LOkhande <apekshalokhande8600@gmail.com>
2024-04-17 12:02:01 +0530

    complete JDBC project on Banking - System

---
+++ src/User.java
@@ -0,0 +1,90 @@
+import java.util.Scanner;
+import java.sql.*;
+public class User {
+    private Connection connection;
+    private  Scanner scanner;
+
+    public User(Connection connection,Scanner scanner){
+        this.connection=connection;
+        this.scanner=scanner;
+    }
+    public void register(){
+        scanner.nextLine();
+        System.out.print("Full name:");
+        String full_name=scanner.nextLine();
+        System.out.print("Email :");
+        String email=scanner.nextLine();
+        System.out.print("Password :");
+        String password=scanner.nextLine();
+        if(user_exist(email)){
+            System.out.println("User already exist for this email address");
+            return;
+        }
+        String register_query="INSERT INTO users(full_name,email,password)VALUES(?,?,?)";
+        try{
+            PreparedStatement preparedStatement=connection.prepareStatement(register_query);
+            preparedStatement .setString(1,full_name);
+            preparedStatement.setString(2,email);
+            preparedStatement.setString(3,password);
+            int affectedRows=preparedStatement.executeUpdate();
+            if(affectedRows>0){
+                System.out.println("Registration successful!");
+            }
+            else{
+                System.out.println("Registration failed");
+            }
+        }catch(SQLException e){
+           e.printStackTrace();
+
+        }
+    }
+
+
+
+    public String login(){
+        scanner.nextLine();
+        System.out.println("Enter your Email");
+        String email=scanner.nextLine();
+        System.out.print("Password :");
+        String password=scanner.nextLine();
+        String login_query="SELECT * FROM users WHERE email=? AND password=?";
+        try{
+            PreparedStatement preparedStatement= connection.prepareStatement(login_query);
+            preparedStatement.setString(1,email);
+            preparedStatement.setString(2,password);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return  email;
+            }
+            else{
+                return null;
+            }
+        }
+        catch(SQLException e){
+            e.printStackTrace();
+
+        }
+        return null;
+    }
+
+public boolean user_exist(String email){
+        String query="SELECT * FROM users WHERE email= ?";
+        try{
+            PreparedStatement preparedStatement= connection.prepareStatement(query);
+            preparedStatement.setString(1,email);
+            ResultSet resultSet=preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return  true;
+            }
+            else{
+                return  false;
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return false;
+}
+
+
+
+}
